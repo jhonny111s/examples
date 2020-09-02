6f5740e345c7520ffc1b4f32c9f3d78a6ec4ae89
jhonny111s <jhonny111s@gmail.com>
2020-09-02 12:21:43 -0500

    se creo socket con modulo net

---
+++ sockets/net/client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+const process = require('process');
+
+// connection to server
+var client = net.connect({port: 8080}, function(c) {
+   console.log('client connected to server');
+});
+
+// received data from server
+client.on('data', function(data) {
+   console.log(data.toString());
+});
+
+client.on('end', function() { 
+    console.log('server disconnected');
+});
+
+// reading console text
+process.stdin.on('data', function(send){
+    const message = send.toString().trim();
+    if (message === 'exit') {
+        client.end();
+    }
+    else {
+        client.write(send);
+    }
+});
+
+// we can do de same with 'telnet localhost 8080'
---
+++ sockets/net/server.js
@@ -0,0 +1,52 @@
+let clients = [];
+const net = require('net');
+
+
+var server = net.createServer(function (connection) {
+    console.log('Some one connected to server');
+    // saved current socket connection
+    clients.push(connection);
+    connection.name = clients.length;
+
+    // amount of connections
+    server.getConnections((error, number) => {
+        console.log(`amount: ${number}`);
+    });
+
+    // server received data
+    connection.on('data', function (data) {
+        const message = data.toString().trim();
+        console.log(`client${connection.name}: ${message}`);
+
+        if (message === 'bye') {
+            // send to everybody
+            clients.forEach((con) => {
+                con.write(`Broadcasting ${message}\n`);
+            });
+        }
+        else {
+            // answered to client
+            connection.write(`server: ${message}\n`);
+        }
+    });
+
+    connection.on('end', function () {
+        // removed client disconected
+        const index = clients.indexOf(connection);
+        console.log('index: '+index);
+        clients.splice(index, 1);
+        console.log('client disconected', clients.length);
+    });
+
+    // server send data
+    connection.write('Welcome client!\r\n');
+    //connection.pipe(connection);
+}).on('error', (err) => {
+    // Handle errors here.
+    throw err;
+});
+
+
+server.listen(8080, function () {
+    console.log('server is listening');
+});
